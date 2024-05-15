a1c3483af32e578d88fb4cac50760ff902cd42d6
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-15 14:48:28 +0200

    Use relation in endpoints GET, POST in orders.service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -7,12 +7,13 @@ export class OrdersService {
   constructor(private prismaService: PrismaService) {}
 
   public getAll(): Promise<Order[]> {
-    return this.prismaService.order.findMany();
+    return this.prismaService.order.findMany({ include: { product: true } });
   }
 
   public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
       where: { id },
+      include: { product: true },
     });
   }
 
@@ -25,8 +26,14 @@ export class OrdersService {
   public create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
+    const { productId, ...otherData } = orderData;
     return this.prismaService.order.create({
-      data: orderData,
+      data: {
+        ...otherData,
+        product: {
+          connect: { id: productId },
+        },
+      },
     });
   }
 
