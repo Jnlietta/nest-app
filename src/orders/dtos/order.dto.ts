d21c715d2ec2360ef6a9c34f408dfd96bd7c7d14
Julia Glinka <julia.w.glinka@gmail.com>
2024-05-17 16:15:49 +0200

    Add data of clients in seeds.ts and update order.dto.ts

--- src/orders/dtos/order.dto.ts
+++ src/orders/dtos/order.dto.ts
@@ -1,19 +1,13 @@
-import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsString, Length, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class OrderDTO {
-  @IsNotEmpty()
-  @IsString()
-  @Length(10, 20)
-  client: string;
-
   @IsNotEmpty()
   @IsString()
   @IsUUID()
   productId: string;
 
-  @IsString()
   @IsNotEmpty()
-  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
-  address: string;
+  @IsUUID()
+  @IsString()
+  clientId: string;
 }
